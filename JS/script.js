0981296b0bb4689e571f7d47ab90ecdd4978f693
HarshitchoudharyITG <harshitchoudhary@itgeeks.com>
2025-10-14 20:06:34 +0530

    change

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,13 @@
-  //  var swiper = new Swiper(".mySwiper", {
-  //     spaceBetween: 30,
-  //     centeredSlides: true,
-  //     autoplay: {
-  //       delay: 2500,
-  //       disableOnInteraction: false,
-  //     },
-  //     pagination: {
-  //       el: ".swiper-pagination",
-  //       clickable: true,
-  //     },
-  //   });
-
-
-    
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
